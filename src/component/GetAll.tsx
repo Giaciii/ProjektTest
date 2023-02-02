2aed9e6e6579af715b4a4c3849292828de51f4af
Giaciii <giacun.masueger@stud.kbw.ch>
2023-02-02 11:32:53 +0100

    delete

--- src/component/GetAll.tsx
+++ src/component/GetAll.tsx
@@ -5,17 +5,21 @@ import axios from 'axios';
 export default function GetAll() {
     const [task, setTask] = useState<Aufgaben[]>([]);
 
-    let all = '';
-    let alles = document.getElementById('alle') as HTMLElement;
-
     useEffect(() => {
+        loadData();
+    }, []);
+
+    function loadData() {
         axios.get<Aufgaben[]>('http://localhost:3001/tasks').then((response) => {
             setTask(response.data);
-        })
-    })
+        });
+    }
 
     function deleteTask(taskToDelete: Aufgaben) {
-        axios.delete("http://localhost:3001/tasks"+taskToDelete.id);
+        axios.delete("http://localhost:3001/task/"+taskToDelete.id).then(() => {
+            
+        });
+    
     }
 
     return (
@@ -25,7 +29,7 @@ export default function GetAll() {
             </form>
             <ol id="alle">
                 {task.map((todo: Aufgaben) => (
-                    <li>{todo.title}</li>
+                    <li>{todo.title} <button onClick={() => deleteTask(todo)}>Delete</button></li>
                 ))}
             </ol>
         </>
